de2f45f2f28e6485dea4147f83ee994a6a9a90ab
Cass <cassandra.dawson1@marist.edu>
2026-05-04 13:44:52 -0400

    Connected Door to Deathbox
    Overview
    - Connected door that allows player to enter mother island interior to change the respawn location of the deathbox

--- 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableDoor.cs
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Interaction/InteractableDoor.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using System;
+using UnityEngine.Events;
 
 public class InteractableDoor : MonoBehaviour
 {
 
     public Transform teleportLocation;
 
+    public UnityEvent<Vector3> doorEntered;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             Player.Instance.TeleportPlayer(teleportLocation.position);
+            doorEntered.Invoke(teleportLocation.position);
         }
     }
 
