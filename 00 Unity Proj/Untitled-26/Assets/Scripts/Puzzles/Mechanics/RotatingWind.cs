a3ee02345440a567efc437c949ef8044b3d5650b
mannyhernandez0 <168492397+mannykun@users.noreply.github.com>
2026-05-11 22:26:11 -0400

    Wind Particles added to every puzzle

---
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/Puzzles/Mechanics/RotatingWind.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class rotatingwind : MonoBehaviour{
+    public Vector3 rotationSpeed = new Vector3(0, 100, 0);
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
