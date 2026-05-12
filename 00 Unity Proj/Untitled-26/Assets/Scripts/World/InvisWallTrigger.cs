166e2d0b32968ae899d69d7b40562955475cf63c
Cass <cassandra.dawson1@marist.edu>
2026-05-12 14:45:43 -0400

    Cumulus Forced Dialogue
    Overview
    - Created an invisible wall script that triggers dialogue directing the player to their objective
    - Added an invisible wall on Mother Island (blocks entry to interior) that disables after talking to Cumulus

---
+++ 00 Unity Proj/Untitled-26/Assets/Scripts/World/InvisWallTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+using Yarn.Unity;
+
+public class InvisWallTrigger : MonoBehaviour
+{
+    public DialogueRunner runner;
+    public string nodeToRun;
+
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasTriggered)
+        {
+            hasTriggered = true;
+            runner.StartDialogue(nodeToRun);
+        }
+    }
+
+}
